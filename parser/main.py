11475a2e8f9ca1b532f22c8208a69dfd7e21252f
Artur Volnov <fristail95@gmail.com>
2024-05-30 00:11:24 +0500

    second commit

--- parser/main.py
+++ parser/main.py
@@ -1,3 +1,4 @@
+from calendar import c
 import time
 import requests
 from selenium import webdriver
@@ -5,7 +6,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    TimeoutException,
+    StaleElementReferenceException,
+)
 from paths import cards, input_field, cookies, count, next_page
 
 
@@ -17,11 +22,7 @@ def make_list_cities() -> list:
         "https://catalog.api.2gis.com/2.0/region/list?fields=items.country_code,items.code&key=028765df-ee43-429d-b281-099f973e3e87"
     )
     response = requests.get(url=url).json()["result"]["items"]
-    cities: list = [
-        (city["name"], city["code"])
-        for city in response
-        if city["country_code"] == "ru"
-    ]
+    cities: list = [city["code"] for city in response if city["country_code"] == "ru"]
     return cities
 
 
@@ -47,22 +48,43 @@ def find_quantity(wait: WebDriverWait) -> int:
         return 0
 
 
+def parsing_page(driver: webdriver, wait: WebDriverWait, city=None, category=None):
+    if city is None:
+        cities: list = make_list_cities()
+        links: list = [f"{URL}/{city}/search/{category}" for city in cities]
+        for link in links:
+            open_page(driver=driver, url=link)
+    else:
+        link: str = f"{URL}/{city}/search/{category}"
+        open_page(driver=driver, url=link)
+        click_on_cookies(driver=driver)
+
+
+    # for counter, city in enumerate(cities, start=1):
+    #     link: str = f"{URL}/{city}/search/{category}"
+    #     open_page(driver=driver, url=link)
+    #     if counter == 1:
+    #         click_on_cookies(driver=driver)
+    #     items = driver.find_elements(by=By.XPATH, value=cards)
+    #     print(link)
+    #     for item in items:
+    #         time.sleep(0.3)
+    #         try:
+    #             item.click()
+    #         except StaleElementReferenceException:
+    #             print("Страница не найдена")
+    #             continue
+    # quantity = find_quantity(wait=wait)
+    # pages = round(quantity / 12 + 0.5)
+
+
 def main():
-    cities = make_list_cities()
+    city: str = "balakovo"
     category: str = "строительный магазин"
     driver = webdriver.Chrome()
-    wait = WebDriverWait(driver, 5)
+    wait = WebDriverWait(driver, 3)
     driver.maximize_window()
-    for counter, city in enumerate(cities, start=1):
-        link: str = f"{URL}/{city[1]}/search/{category}"
-        open_page(driver=driver, url=link)
-        if counter == 1:
-            click_on_cookies(driver=driver)
-        quantity = find_quantity(wait=wait)
-        pages = round(quantity / 12 + 0.5)
-        for page in range(1, pages + 1):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            wait.until(EC.presence_of_element_located((By.XPATH, next_page))).click()
+    parsing_page(driver=driver, wait=wait, category=category)
     driver.quit()
 
 
